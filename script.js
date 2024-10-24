4283abdc2985f8a925b68ac62ad8f8d4e95524e8
Sizex03 <tiagoms244@gmail.com>
2024-10-24 19:19:24 -0300

    att

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let currentIndex = 0;
 function moveCarousel(direction, trackId) {
     const track = document.getElementById(trackId);
     const totalProducts = track.children.length;
-    const productsToShow = 5; // Número de produtos exibidos de cada vez
+    const productsToShow = 3; // Número de produtos exibidos de cada vez
     const maxIndex = Math.ceil(totalProducts / productsToShow) - 1;
 
     currentIndex += direction;
@@ -17,6 +17,7 @@ function moveCarousel(direction, trackId) {
     const offset = -currentIndex * (100 / productsToShow);
     track.style.transform = `translateX(${offset}%)`;
 }
+
 let cartCount = 0; // Variável para contar itens no carrinho
 function addToCart(productName) {
     cartCount++; // Incrementa o contador de itens no carrinho
