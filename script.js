6d9c3d3b68a06496b599e44bea989c62ad9d98f1
Sizex03 <tiagoms244@gmail.com>
2024-10-24 05:23:29 -0300

    alterações

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-const header = document.querySelector("header");
+let currentIndex = 0;
 
-window.addEventListener ("scroll", function(){
-    header.classList.toggle ("sticky", this.window.scrollY > 0);
-})
+function moveCarousel(direction, trackId) {
+    const track = document.getElementById(trackId);
+    const totalProducts = track.children.length;
+    const productsToShow = 5; // Número de produtos exibidos de cada vez
+    const maxIndex = Math.ceil(totalProducts / productsToShow) - 1;
 
-let menu = document.querySelector('#menu-icon');
-let navmenu = document.querySelector('.navmenu');
+    currentIndex += direction;
 
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navmenu.classList.toggle('open');
+    if (currentIndex < 0) {
+        currentIndex = 0;
+    } else if (currentIndex > maxIndex) {
+        currentIndex = maxIndex;
+    }
+
+    const offset = -currentIndex * (100 / productsToShow);
+    track.style.transform = `translateX(${offset}%)`;
 }
+let cartCount = 0; // Variável para contar itens no carrinho
+function addToCart(productName) {
+    cartCount++; // Incrementa o contador de itens no carrinho
+    document.getElementById('cart-count').textContent = cartCount; // Atualiza o contador no botão
+    alert(`${productName} foi adicionado ao carrinho!`);
+}
+
 
