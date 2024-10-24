33c044face622eed7ca207caeb5775db2013967c
Gabstaudt <macstaudt@gmail.com>
2024-10-24 20:09:15 -0300

    remoção

--- scriptEspecifico.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('addToCart').addEventListener('click', function() {
-    const quantity = document.getElementById('quantity').value;
-    alert(`Você adicionou ${quantity} unidade(s) do produto ao carrinho!`);
-});
